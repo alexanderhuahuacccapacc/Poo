f141534fb7a6e026797c26daef0fb8fb5f979646
alexanderhuahuacccapacc <142243666+alexanderhuahuacccapacc@users.noreply.github.com>
2024-04-19 14:56:07 -0500

    Sube archivoss

---
+++ Principiospoo/src/main/java/pe/edu/upeu/poo/arrayst/Persona.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.poo.arrayst;
+
+/**
+ *
+ * @author 51960
+ */
+public class Persona {
+    private String dni;
+    private String Nombre;
+    private int edad;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    void setdni(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
