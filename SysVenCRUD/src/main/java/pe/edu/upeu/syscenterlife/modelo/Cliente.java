1ef962f8629fbbc5f04f7e4057b3d30b3d479870
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2024-04-22 09:31:15 -0500

    hoal

---
+++ SysVenCRUD/src/main/java/pe/edu/upeu/syscenterlife/modelo/Cliente.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.modelo;
+
+/**
+ *
+ * @author Datos
+ */
+import lombok.Data;
+
+@Data
+public class Cliente {
+
+    private String dniruc;
+    private String nombres;
+    private String documento;
+}
