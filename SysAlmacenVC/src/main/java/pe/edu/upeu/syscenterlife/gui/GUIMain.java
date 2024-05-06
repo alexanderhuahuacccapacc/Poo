e4c02b8c3935956eb8d92dc9a02c9a17c7ffb52c
alexanderhuahuacccapacc <142243666+alexanderhuahuacccapacc@users.noreply.github.com>
2024-05-06 10:43:50 -0500

    hioll

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/gui/GUIMain.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 51960
+ */
+public class GUIMain extends JFrame {
+    
+}
