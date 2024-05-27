3c7111260db8c46f6a1549b5d6e37b8215cae828
Mark-Pino <mark.pino@upeu.edu.pe>
2024-05-27 09:04:30 -0500

    lunes27-9

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/SessionManager.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Datos
+ */
+@Data
+//Siempre que este @data siempre es en modelo 
+public class SessionManager {
+
+    static SessionManager instance;
+    Long userId;
+
+    public static synchronized SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+}
